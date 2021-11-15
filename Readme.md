62da8f54dfb3f4a6d8b134babb3b7989d74afee8
Gabriel Marmore <gabrielpiraja@hotmail.com>
2021-11-14 21:55:43 -0300

    feat/forms.php

---
+++ Readme.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+  <a href="https://formsrecaptcha.azurewebsites.net/index.html">🔗Formulário + CAPTCHA</a>
+</h1>
+<p>Trabalho da faculdade para integrar um formulário com <strong>CAPTCHA</strong>. Utilizei a API do Google <strong>reCAPTCHA</strong></p>
+
+<h2>Tecnologias</h2>
+
+<p>As seguintes ferramentas foram usadas na construção do projeto:</p>
+<ul>
+  <li><strong>PHP</strong>;</li>
+  <li><strong>HTML</strong>;</li>
+  <li><strong>CSS</strong>.</li>
+</ul>
---
+++ forms.php
@@ -0,0 +1,48 @@
+<?php
+if (isset($_POST['user'])) {
+    $nome = $_POST['user'];
+}
+
+if (isset($_POST['pin'])) {
+    $mensagem = $_POST['pin'];
+}
+
+if (isset($_POST['g-recaptcha-response'])) {
+    $captcha_data = $_POST['g-recaptcha-response'];
+}
+
+// Se nenhum valor foi recebido, o usuário não realizou o captcha
+if (!$captcha_data) {
+    $msg = "Por favor, confirme o captcha.";
+    exit($msg);
+}
+
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfVDzUdAAAAAPx1qtdhAVCv2apvBh5xuFFwRfU_&response=".$captcha_data."&remoteip=".$_SERVER['REMOTE_ADDR']);
+$responseData = json_decode($response, true);
+
+if ($responseData["success"] == true) {
+    $msg = "Formulário enviado com sucesso!";
+} else {
+    $msg = "Usuário mal intencionado detectado. Formulário não foi enviada.";
+    exit($msg);
+}
+?>
+<!DOCTYPE html><html lang="pt-br"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Response forms</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<main>
+
+<h1>Formulário + CAPTCHA</h1>
+<h2>Response</h2>
+
+<div>
+   <?= $msg;?>
+
+   <a class="botao" href="index.html">Voltar</a>
+</div>
+
+</main>
+</body>
+</html>
